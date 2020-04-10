ab64c19a2044a48c3af86fe1270261d5604e77ff
David Enoma <davidenoma@gmail.com>
2020-04-10 11:04:47 +0100

    Dashboard Update

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -52,7 +52,7 @@
                 <a class="block block-rounded" href="{{url('/user/chat')}}">
                         <div class="block-content block-content-full">
                             <div class="py-20 text-center" style="color: #008000;">
-                                <div class="font-size-h2 font-w700 mb-0 text-primary-dark">Messaging</div>
+                                <div class="font-size-h2 font-w700 mb-0 text-primary-dark">Chat</div>
                                 <i class="fa fa-inbox fa-2x"></i>
 
                             </div>
