5ca038e9d5939df239c15a1149fafe58cf4d0fea
David Enoma <davidenoma@gmail.com>
2020-06-11 19:08:12 +0100

    positioning the footer properly

--- resources/views/registration.blade.php
+++ resources/views/registration.blade.php
@@ -122,7 +122,7 @@ SmallJobsNaija- Talent Registration Form
        
     </div>
     <div class="form-body" style="height:40%"></div>
-    <footer class="footer" style="background-color: white; color: #008000;padding-top: 0;">
+    <footer class="footer" style="background-color: white; color: #008000;padding-top: 0;bottom: 0">
         <br>
         <div class="wrapper">
             <div class="container" style="color:#008000;padding-top:0">
